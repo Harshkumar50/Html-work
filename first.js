9f861857e983f8f1409187e74fb6eba71e2d9405
Harsh kumar <115373712+Harshkumar50@users.noreply.github.com>
2024-05-11 23:45:13 +0530

    Create first.js

---
+++ first.js
@@ -0,0 +1,19 @@
+console.log("harsh kumar");
+console.log("refresh it");
+x=null;
+y="I am a string";
+console.log(x);
+fullname=34;
+console.log(fullname);
+//variables are case sensetive 
+//no space allowed b/w letter
+//var: can be re-declared and updated // global scope
+//let: variables cannot be re-declared but can be updated //block scope
+//const: not re-declared and updated // block scope
+const m=233;
+console.log(m);
+const student={
+    name:"Harsh",
+    age:20,
+};
+console.log(student.age);// to access the object key or student["age"]
