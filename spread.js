5734604960fc2335eca150a84fea7b89d82ff8e7
Harsh kumar <115373712+Harshkumar50@users.noreply.github.com>
2024-08-26 19:37:12 +0530

    Create spread.js

---
+++ spread.js
@@ -0,0 +1,4 @@
+let frontendcourses = [ 'HTML','CSS','React js','Vanilla js'];
+let backendcourses = ['Node js','Express js','Mongo db','SQL'];
+let combinedcourses = [...frontendcourses, ...backendcourses];
+console.log(combinedcourses);
